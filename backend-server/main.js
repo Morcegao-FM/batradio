c3777237d41fa1fb6bc83d33546e56020e4b0a89
André Guergolet <aguergolet@gmail.com>
2021-03-29 02:47:07 -0300

    get current playlist into memory

--- backend-server/main.js
+++ backend-server/main.js
@@ -1,6 +1,45 @@
-var mpd = require('mpd')
-var fs = require('fs')
+var mpd = require("mpd");
+var fs = require("fs");
 
-var config =JSON.parse(  fs.readFileSync('config.json') );
+var config = JSON.parse(fs.readFileSync("config.json"));
+var currentPlaylist = Array();
 
-console.log( config.mpd.server );
+console.log("Connecting");
+
+var client = mpd.connect({
+  host: config.mpd.server,
+  port: config.mpd.port,
+  password: config.mpd.password
+});
+
+var start = function() {
+  getPlaylist();  
+};
+
+var error = function(err) {
+  console.log(err);
+};
+
+function promisedGetPlaylist() {
+  return new Promise((resolve, reject) => {    
+    client.sendCommand(mpd.cmd("playlistinfo", []), (err, msg) => {
+        console.log('playlistinfo required');
+      if (err) 
+        console.error("err " + err);
+      msg = msg.split("\nfile:");
+      msg.forEach((item) => {  currentPlaylist.push( mpd.parseKeyValueMessage('file: ' + item)); });
+      resolve();
+      console.log('playlistinfo finished');
+    });    
+  });
+}
+
+async function getPlaylist() {  
+  await promisedGetPlaylist().then(() => {
+    console.log(currentPlaylist[0]);
+  });
+  
+}
+
+client.on("error", error);
+client.on("ready", start);
